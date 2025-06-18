f818cc80df8ceefae2f086575593e79539f6b597
gulerzehra <zehraaguleer08@gmail.com>
2025-06-18 03:47:45 +0300

    günlere ulaşıldı

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { defineCustomElements } from "@siemens/ix/loader";
 import { defineCustomElements as defineIxIconCustomElements } from "@siemens/ix-icons/loader";
+import moment from "moment";
+import "moment/locale/tr";
 
 defineIxIconCustomElements();
 defineCustomElements();
@@ -29,23 +31,35 @@ window.addEventListener("DOMContentLoaded", async () => {
   const hydrated = dateDropdown.querySelectorAll(".hydrated");
   console.log("hydrated elements:", hydrated);
   console.log(hydrated[3]);
+  // console.log(hydrated[4]); ->burası done buton
 
   const card = hydrated[3].shadowRoot;
   console.log("Card shadowRoot:", card);
 
-  const day = card.querySelectorAll(".calendar-item");
-  console.log("Grid element:", day);
+  const grid = card.querySelectorAll(".calendar-item");
+  console.log("Grid element:", grid);
+
+  const dayCells = card.querySelectorAll('[id^="day-cell"]');
+  dayCells.forEach((cell) => {
+    console.log(cell.id, cell.textContent);
+  }); //burada bütün günleri çektim
 
   console.log(hydrated);
   hydrated[3].addEventListener("click", () => {
     console.log("✅ Hydrated element clicked!");
   });
 
-  day.forEach((item) => {
+  // // Ya da Array.from ile gerçek bir diziye çevirip işle:
+  // const items = Array.from(grid);
+  // items.map((cell) => cell.id); // ["day-cell-1", "day-cell-2", ...]
+
+  grid.forEach((item) => {
     item.addEventListener("click", () => {
-      console.log("✅ Day clicked:", item);
-      const a = dropdown.getDateRange();
-      console.log(a);
+      setTimeout(async () => {
+        dropdown.getDateRange().then(({ from, to }) => {
+          console.log("▶ from, to =", from, to);
+        });
+      }, 0);
     });
   });
 });
